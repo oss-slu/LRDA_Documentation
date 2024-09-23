8e622a0a39019d122399b9c41074a792876cc4a6
Yash Kamal Bhatia <113057400+yashb196@users.noreply.github.com>
2024-09-23 16:42:10 -0500

    adding changes required

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -9,7 +9,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://LRDA_Documentation.com',
+  url: ' https://oss-slu.github.io/LRDA_Documentation/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
